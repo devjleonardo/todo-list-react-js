65cb68d6ca2e3984bebac9300910b1dc1f24f5ab
devjleonardo <leonardo_cordeirobahia@hotmail.com>
2025-10-17 01:05:14 -0300

    Adicionando router e concluindo o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 import { v4 } from "uuid";
+import Title from "./components/Title";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar programação",
-      description:
-        "Estudar programação para se tornar desenvolvedor full stack",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: "Ler livros",
-      description: "Ler livros de desenvolvimento pessoal",
-      isCompleted: true,
-    },
-    {
-      id: 3,
-      title: "Exercitar",
-      description: "Fazer exercícios físicos para manter a saúde em dia",
-      isCompleted: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        {
+          method: "GET",
+        }
+      );
+      const data = await response.json();
+
+      setTasks(data);
+    };
+
+    // Chamamos uma API para pegar as tasks
+    // fetchTasks();
+  }, []);
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
@@ -63,9 +67,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">
-          Gerenciador de tarefas
-        </h1>
+        <Title>Gerenciador de tarefas</Title>
         <AddTask onAddTaskSubmit={onAddTaskSubmit} />
         <Tasks
           tasks={tasks}
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Input from "./Input";
 
 function AddTask({ onAddTaskSubmit }) {
   const [title, setTitle] = useState("");
@@ -6,29 +7,26 @@ function AddTask({ onAddTaskSubmit }) {
 
   return (
     <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
-      <input
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
+      <Input
         type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
         placeholder="Digite o título da tarefa"
-        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
       />
 
-      <input
-        onChange={(e) => setDescription(e.target.value)}
-        value={description}
+      <Input
         type="text"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
         placeholder="Digite a descrição da tarefa"
-        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
       />
 
       <button
         onClick={() => {
-          // verificar  
+          // verificar
           if (!title.trim() || !description.trim()) {
             alert("Por favor, preencha todos os campos");
             return;
-
           }
           onAddTaskSubmit(title, description);
           setTitle("");
